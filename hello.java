44f12f063a92bb14e3e42f72557890d8efa6dbb7
peng <675163401@qq.com>
2021-03-27 10:49:52 +0800

    编写person类

---
+++ hello.java
@@ -0,0 +1,9 @@
+public class Person{
+	private int age;
+	private String name;
+	public static void main(String[] args){
+		Person preson = new Person();
+		person.setAge(30);
+		person.setName("lisi");
+	}
+}
